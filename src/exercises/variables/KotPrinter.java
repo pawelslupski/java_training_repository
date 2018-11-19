651cd20c045d49fe3140a688d164b309574c7a0e
pawelslupski <pawelslupski.go@gmail.com>
2018-11-19 20:45:15 +0100

    variables_exercises

---
+++ src/exercises/variables/KotPrinter.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class KotPrinter {
+    public static void main(String[] args) {
+        char K = 75;
+        char O = 79;
+        char T = 84;
+        System.out.print(K);
+        System.out.print(O);
+        System.out.print(T);
+    }
+}
